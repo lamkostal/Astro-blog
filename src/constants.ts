c78aa3ca465a7320877b788e0269639af23f2d02
lamkostal <lamkostal@gmail.com>
2024-05-24 17:03:54 +0300

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "TechPeople blog"
+export const SITE_DESCRIPTION = 'Articles, stories and tutorials from tech people.'
+export const HOMEPAGE_ARTICLE_LIMIT =8
+export const ARTICLES_PER_PAGE = 6
