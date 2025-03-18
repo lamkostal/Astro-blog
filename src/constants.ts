2d95dfacad24dd66135b012970561049fda8dfba
lamkostal <lamkostal@gmail.com>
2025-03-18 18:22:51 +0200

    layout change

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "Φυτά Εντος"
 export const SITE_DESCRIPTION = 'Φυτά Εντος, ένα blog για λάτρεις των φυτών εσωτερικού χώρου.'
-export const HOMEPAGE_ARTICLE_LIMIT =8
+export const HOMEPAGE_ARTICLE_LIMIT =6
 export const ARTICLES_PER_PAGE = 12
